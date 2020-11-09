7ae631ebd9e7b9f68b4461136676c5b5cdfefe3b
NamDoan <dnhnam91@gmail.com>
2020-11-09 08:04:24 +0700

    Initial Commit

---
+++ Scroll+TableViewZalo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scroll+TableViewZalo
+//
+//  Created by namit on 11/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
